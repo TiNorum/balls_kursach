613126945ae36cf003cb8e81c08e0c216bf68657
Professional <tnorum@yandex.ru>
2021-05-15 20:53:57 +0300

    Revert "Добавьте файлы проекта."
    This reverts commit 5423f9e2b9d919b7b9654da35d61a2cac90078c6.

--- balls_kursach/records.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iostream>
-
-void leaderboardSort(int* leaderboard);
-void leaderboardWrite(int* leaderboard);
-void leaderboardRead(int* leaderboard);
-void leaderboardUpdate(int* leaderboard, int score);
